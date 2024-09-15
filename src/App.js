941f1dd31261eed926fb8101cc4b2e049598e2e0
nitesh545 <hitman12457868@gmail.com>
2024-09-15 17:38:42 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let [flags, setFlags] = useState([]);
+    let [searchText, setSearchText] = useState("");
+    let [filteredFlags, setFilteredFlags] = useState([]);
+
+    let fetchFlags = () => {
+        try {
+            fetch("https://restcountries.com/v3.1/all")
+                .then((response) => response.json())
+                .then((data) => setFlags(data));
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    let searchFlags = () => {
+        try {
+            fetch(`https://restcountries.com/v3.1/all/${searchText}`)
+                .then((response) => response.json())
+                .then((data) => setFlags(data));
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    let showFilteredFlags = () => {
+        setFilteredFlags(
+            flags.filter((x) => x.name.common.includes(searchText))
+        );
+    };
+
+    useEffect(() => {
+        fetchFlags();
+    }, []);
+
+    return (
+        <div className="App">
+            <input
+                type="search"
+                onChange={(event) => {
+                    setSearchText(event.target.value);
+                    // showFilteredFlags();
+                }}
+            ></input>
+            <div
+                style={{
+                    display: "flex",
+                    flexWrap: "wrap",
+                }}
+            >
+                {flags &&
+                    flags.map((flag) => {
+                        return (
+                            <div
+                                key={crypto.randomUUID()}
+                                className="countryCard"
+                                style={{ padding: "0.5rem" }}
+                            >
+                                <img
+                                    src={flag.flags.png}
+                                    alt=""
+                                    style={{
+                                        width: "100%",
+                                        verticalAlign: "middle",
+                                    }}
+                                ></img>
+                                <h5>{flag.name.common}</h5>
+                            </div>
+                        );
+                    })}
+            </div>
+        </div>
+    );
 }
 
 export default App;
